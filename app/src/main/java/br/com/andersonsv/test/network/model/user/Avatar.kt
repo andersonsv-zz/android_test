8ce4f99f198dda25dd299293cf5ce789f6fbe629
Your Name <anderson.si.vieira@hotmail.com>
2019-04-20 17:13:53 -0300

    Redução do boilerplate de parcelable

--- app/src/main/java/br/com/andersonsv/test/network/model/user/Avatar.kt
+++ app/src/main/java/br/com/andersonsv/test/network/model/user/Avatar.kt
@@ -1,36 +1,13 @@
 package br.com.andersonsv.test.network.model.user
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Avatar(
     @field:Json(name = "public_id")
     val publicId: String,
-    @field:Json(name = "crop")
     val crop: String,
-    @field:Json(name = "gravity")
     val gravity: String
-) : Parcelable {
-    constructor(source: Parcel) : this(
-        source.readString(),
-        source.readString(),
-        source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(publicId)
-        writeString(crop)
-        writeString(gravity)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<Avatar> = object : Parcelable.Creator<Avatar> {
-            override fun createFromParcel(source: Parcel): Avatar = Avatar(source)
-            override fun newArray(size: Int): Array<Avatar?> = arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
